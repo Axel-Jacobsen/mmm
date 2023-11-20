2726c16db18a8d0b4f9bcf96358de2991f8d6cdd
Axel Jacobsen <axelnj44@gmail.com>
2023-11-20 10:18:37 -0800

    init

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+use std::collections::HashMap;
+
+use reqwest;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let resp =
+        reqwest::blocking::get("https://httpbin.org/ip")?.json::<HashMap<String, String>>()?;
+
+    println!("{:#?}", resp);
+    Ok(())
+}
