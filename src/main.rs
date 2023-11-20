4b5727697fb5cd4823ea7efedebd6a7db1dc218e
Axel Jacobsen <axelnj44@gmail.com>
2023-11-20 11:33:10 -0800

    fx

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,22 @@
-use std::collections::HashMap;
+use std::env;
 
 use reqwest;
 
+fn get_api_key() -> Result<String, env::VarError> {
+    match env::var("MANIFOLD_KEY") {
+        Ok(key) => Ok(format!("Key {key}")),
+        Err(e) => Err(e),
+    }
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let resp =
-        reqwest::blocking::get("https://httpbin.org/ip")?.json::<HashMap<String, String>>()?;
+    let client = reqwest::blocking::Client::new();
+
+    let req = client
+        .get("https://manifold.markets/api/v0/me")
+        .header("Authorization", get_api_key()?);
+
+    let resp = req.send()?;
 
     println!("{:#?}", resp);
     Ok(())
