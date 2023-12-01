56028e0ceef6ba2148ebb9765e0eca20e8dd1e76
Axel Jacobsen <axelnj44@gmail.com>
2023-11-30 20:32:17 -0800

    ok struvturescturilaksd

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 mod market_handler;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let market_handler = market_handler::MarketHandler::new(vec![String::from("/v0/bets")]);
+    let market_handler = market_handler::MarketHandler::new(vec![String::from("bets")]);
     println!("{:?}", market_handler);
+    market_handler.run();
     Ok(())
 }
--- src/market_handler.rs
+++ src/market_handler.rs
@@ -14,32 +14,15 @@ fn get_env_key(key: &str) -> Result<String, String> {
     }
 }
 
-// let client = reqwest::blocking::Client::new();
-
-// let req = client
-//     .get("https://manifold.markets/api/v0/me")
-//     .header("Authorization", market_handler::get_api_key()?);
-
-// println!("REQ {req:?}\n");
-
-// let resp = req.send()?;
-
-// match resp.json::<manifold_types::LiteUser>() {
-//     Ok(user) => println!("{user:?}"),
-//     Err(e) => {
-//         let req2 = client
-//             .get("https://manifold.markets/api/v0/me")
-//             .header("Authorization", market_handler::get_api_key()?);
-//         println!("{e} for text {:#?}", req2.send()?.text())
-//     }
-// }
-
 #[allow(dead_code)]
 #[derive(Debug)]
 pub struct MarketHandler {
     api_key: String,
-    endpoints: Vec<String>,
     api_url: String,
+    endpoints: Vec<String>,
+
+    api_read_limit_per_s: u32,
+    api_write_limit_per_min: u32,
 }
 
 #[allow(dead_code)]
@@ -51,19 +34,49 @@ impl MarketHandler {
             api_key,
             endpoints,
             api_url: String::from("https://api.manifold.markets"),
+            api_read_limit_per_s: 100,
+            api_write_limit_per_min: 10,
         }
     }
 
-    fn check_alive(&self) -> bool {
+    fn get_endpoint(
+        &self,
+        endpoint: String,
+        query_params: Option<&[(&str, &str)]>,
+    ) -> Result<reqwest::blocking::Response, reqwest::Error> {
         let client = reqwest::blocking::Client::new();
 
-        let req = client
-            .get("https://manifold.markets/api/v0/me")
+        let mut req = client
+            .get(format!("https://manifold.markets/api/v0/{}", endpoint))
             .header("Authorization", get_env_key("MANIFOLD_KEY").unwrap());
 
-        let resp = req.send().unwrap();
+        if let Some(p) = query_params {
+            req = req.query(&p);
+        }
+
+        req.send()
+    }
+
+    pub fn check_alive(&self) -> bool {
+        let resp = self.get_endpoint(String::from("me"), None).unwrap();
 
         resp.json::<manifold_types::LiteUser>().is_ok()
+    }
+
+    pub fn run(&self) {
+        loop {
+            for endpoint in &self.endpoints {
+                std::thread::sleep(std::time::Duration::from_secs(1) / self.api_read_limit_per_s);
+                let resp = self
+                    .get_endpoint(endpoint.to_string(), Some(&[("limit", "1")]))
+                    .unwrap();
+                if resp.status().is_success() {
+                    println!("{:?}", resp.text());
+                } else {
+                    println!("endpoint {endpoint} failed {:?}", resp);
+                }
+            }
+        }
     }
 }
 
