7b3736be35d85d5c8550a91d7d52a6337f7ed719
Axel Jacobsen <axelnj44@gmail.com>
2023-11-30 21:24:41 -0800

    mas

--- src/market_handler/manifold_types.rs
+++ src/market_handler/manifold_types.rs
@@ -6,6 +6,13 @@ pub struct LiteUsers {
     users: Vec<LiteUser>,
 }
 
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(untagged, rename_all = "UPPERCASE")]
+enum MarketType {
+    CPMM(f64),
+    DPM(f64),
+}
+
 #[allow(dead_code)]
 #[derive(Serialize, Deserialize, Debug)]
 pub struct LiteUser {
@@ -40,4 +47,89 @@ pub struct LiteUser {
 
     #[serde(rename = "totalPnLCached")]
     total_pnl_cached: Option<f64>,
+}
+
+#[allow(dead_code)]
+#[derive(Serialize, Deserialize, Debug)]
+pub struct LiteMarket {
+    /// from https://docs.manifold.markets/api#get-v0markets
+
+    /// Unique identifer for this market
+    id: String,
+
+    /// Attributes about the creator
+    #[serde(rename = "creatorUsername")]
+    creator_username: String,
+
+    /// The name of the creator
+    #[serde(rename = "creatorName")]
+    creator_name: String,
+
+    /// milliseconds since epoch
+    #[serde(rename = "creatorAvatarUrl")]
+    created_time: u64,
+    creator_avatar_url: Option<String>,
+
+    /// Market attributes. All times are in milliseconds since epoch
+    /// Min of creator's chosen date, and resolutionTime
+    #[serde(rename = "startTime")]
+    close_time: Option<u64>,
+
+    /// The question!
+    question: String,
+
+    /// Note: This url always points to https://manifold.markets, regardless of what instance the api is running on.
+    /// This url includes the creator's username, but this doesn't need to be correct when constructing valid URLs.
+    ///   i.e. https://manifold.markets/Austin/test-market is the same as https://manifold.markets/foo/test-market
+    url: String,
+
+    #[serde(rename = "outcomeType")]
+    /// BINARY, FREE_RESPONSE, MULTIPLE_CHOICE, NUMERIC, or PSEUDO_NUMERIC
+    outcome_type: String,
+
+    /// dpm-2 or cpmm-1
+    mechanism: String,
+
+    /// current probability of the market
+    probability: f64,
+    /// For CPMM markets, the number of shares in the liquidity pool. For DPM markets, the amount of mana invested in each answer.
+    pool: MarketType,
+    /// CPMM markets only, probability constant in y^p * n^(1-p) = k
+    p: Option<f64>,
+
+    /// CPMM markets only, the amount of mana deposited into the liquidity pool
+    #[serde(rename = "total_liquidity")]
+    total_liquidity: Option<f64>,
+
+    /// PSEUDO_NUMERIC markets only, the current market value, which is mapped from probability using min, max, and isLogScale.
+    value: Option<f64>,
+    /// PSEUDO_NUMERIC markets only, the minimum resolvable value
+    min: Option<f64>,
+    /// PSEUDO_NUMERIC markets only, the maximum resolvable value
+    max: Option<f64>,
+    /// PSEUDO_NUMERIC markets only, if true `number = (max - min + 1)^probability + minstart - 1`, otherwise `number = min + (max - min) * probability`
+    #[serde(rename = "isLogScale")]
+    is_log_scale: Option<bool>,
+
+    volume: f64,
+    #[serde(rename = "volume24Hours")]
+    volume_24_hours: f64,
+
+    #[serde(rename = "isResolved")]
+    is_resolved: bool,
+    #[serde(rename = "resolutionTime")]
+    resolution_time: Option<f64>,
+    resolution: Option<String>,
+
+    /// Used for BINARY markets resolved to MKT
+    #[serde(rename = "resolutionProbability")]
+    resolution_probability: Option<f64>,
+    #[serde(rename = "uniqueBettorCount")]
+    unique_bettor_count: u64,
+
+    #[serde(rename = "lastUpdatedTime")]
+    last_updated_time: Option<f64>,
+
+    #[serde(rename = "lastBetTime")]
+    last_bet_time: Option<f64>,
 }
